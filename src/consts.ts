f52cb4f8f480897814c39b30c87a0abc7a9e9581
JunedKhan <juned7734@gmail.com>
2026-06-14 17:33:41 +0530

    some fix

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
 export const SITE_TITLE = 'Salesforce Dev Notes';
 export const SITE_DESCRIPTION = 'Salesforce Dev Notes';
